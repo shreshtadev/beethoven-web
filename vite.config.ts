929a0c336b4d912131b26d67397cb9490a429b98
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-05-01 17:12:01 +0530

    updated vite config for cors

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,25 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:8090',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+				//  Add headers for /api requests
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+					'Access-Control-Allow-Headers': 'Authorization, Content-Type'
+				}
+			}
+		}
+	},
+	build: {
+		rollupOptions: {
+			external: ['pocketbase'] //  Important:  Prevent PocketBase from being included in the client-side bundle.
+		}
+	}
 });
