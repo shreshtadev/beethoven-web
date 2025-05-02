d16d1eaec2c3440e8638e6213cee1ec6788e0e79
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-05-02 21:06:07 +0530

    updated proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://3.109.250.92:8090',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
-				//  Add headers for /api requests
-				headers: {
-					'Access-Control-Allow-Origin': 'http://localhost:3000',
-					'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-					'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-                    'Access-Control-Allow-Credentials': 'true'
-				}
-			}
-		}
-	},
-
 });
